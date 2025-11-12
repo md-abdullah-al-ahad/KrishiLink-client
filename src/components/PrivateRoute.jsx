7a12b54cc6689ef66e8c08a531c4b156f825a3cb
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-11-13 03:08:59 +0600

    add private router

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-green-600"></div>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
+};
+
+export default PrivateRoute;
